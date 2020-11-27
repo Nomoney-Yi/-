01fb71c679b6b2132663cc3bba160d6c5ede157e
wwy <wwy@qq.com>
2020-11-27 15:14:51 +0800

    首页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,11 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(option) {
     option.url = "http://ajax.frontend.itheima.net" + option.url;
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
 })
